f3b1e60c3f370818c057a104fc03853a3c057374
Rafoolin <niloo.jv@gmail.com>
2023-11-21 00:31:00 +0100

    feat(bash): add bash script for the pipeline

--- project/pipeline.sh
+++ project/pipeline.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
-python3 ./project/pipeline/pipeline.py
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m'
+
+script_dir="$(dirname "$0")"
+echo -e "$GREEN starting...$NC"
+rm -f -r ".etl_pipeline"
+rm -f "./data/"*.csv "./data/"*.tsv "./data/"*.sqlite
+echo -e "$YELLOW Script directory name: $script_dir $NC"
+echo -e "$GREEN Install virtual environment(20.24.6) requirements...$NC"
+pip install virtualenv==20.24.6
+echo -e "$GREEN Create a virtual environment called [.etl_pipeline]...$NC"
+# The directory of the script
+virtualenv "$script_dir/.etl_pipeline"
+source "$script_dir/.etl_pipeline/bin/activate"
+echo -e "$GREEN Install requirements.txt...$NC"
+pip install -r "$script_dir/pipeline/requirements.txt"
+echo -e "$GREEN All requirements are installed successfully!$NC"
+echo -e "$GREEN Pipeline started...$NC"
+python3 "$script_dir/pipeline/pipeline.py"
+echo -e "$GREEN Pipeline Finished$NC"
+deactivate
+echo -e "$GREEN Deactivated the virtual environment$NC"
