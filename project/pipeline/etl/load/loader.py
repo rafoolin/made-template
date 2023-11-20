e705b0fc26bae06dc8045892e63bff66a7a876e7
Rafoolin <niloo.jv@gmail.com>
2023-11-20 23:53:04 +0100

    feat(pipeline): finish pipeline v0.1.0
    add transformers and loaders

---
+++ project/pipeline/etl/load/loader.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import sqlite3
+
+
+def mergeDataToSQL(sdg_data, road_data, tran_data) -> pd.DataFrame:
+    # Merge based on the time and country code
+    merge_on = ["geo", "TIME_PERIOD", 'unit']
+    sdg_tran_data = pd.merge(sdg_data, tran_data, on=merge_on, how="inner")
+    merged_data = pd.merge(road_data, sdg_tran_data, on=merge_on, how="inner")
+    return merged_data.dropna()
+
+
+def loadDataToSQL(data_frame: pd.DataFrame, db_name, table_name):
+    import pipeline_utils.utils as util
+    cwd = util.get_directory_absolute_path()
+    path = f"{cwd}/data/{db_name}.sqlite"
+    conn = sqlite3.connect(path)
+    data_frame.to_sql(f"{table_name}", conn, index=False, if_exists="replace")
+    conn.commit()
+    conn.close()
--- project/pipeline/pipeline.py
+++ project/pipeline/pipeline.py
@@ -1,12 +1,50 @@
+import etl.extract.sdg_extractor as sdg_e
+import etl.extract.geo_extractor as geo_e
+import etl.extract.tran_r_vehst_extractor as tran_e
+import etl.extract.road_eqr_carpda_extractor as road_e
+import etl.transform.road_eqr_carpda_transformer as road_t
+import etl.transform.tran_r_vehst_transformer as tran_t
+import etl.transform.sdg_transformer as sdg_t
+import etl.load.loader as loader
 
-import  etl.extract.sdg_extractor as sdg
-import  etl.extract.geo_extractor as geo
-import  etl.extract.tran_r_vehst_extractor as tran
-import  etl.extract.road_eqr_carpda_extractor as road
 
-# TODO:: Cover try-catch and print/log
-sdg_data_frame = sdg.sdg_data_extractor()
-geo_data_frame = geo.geo_data_extractor()
-tran_data_frame = tran.tran_r_vehst_data_extractor()
-road_data_frame = road.road_eqr_carpda_data_extractor()
+class pipeline:
+    def __extract(self):
+        # TODO:: Cover try-catch and print/log
+        self.sdg = sdg_e.sdg_data_extractor()
+        self.geo = geo_e.geo_data_extractor()
+        self.tran = tran_e.tran_r_vehst_data_extractor()
+        self.road = road_e.road_eqr_carpda_data_extractor()
 
+    def __transform(self):
+        self.cleaned_road = road_t.road_eqr_carpda_data_transformer(self.road)
+        self.cleaned_tran = tran_t.tran_r_vehst_data_transformer(self.tran)
+        self.cleaned_sdg = sdg_t.sdg_data_transformer(self.sdg)
+
+    def __merge(self):
+        self.merged_data = loader.mergeDataToSQL(
+            sdg_data=self.cleaned_road,
+            road_data=self.cleaned_road,
+            tran_data=self.cleaned_road,
+        )
+
+    def __load(self):
+        loader.loadDataToSQL(
+            data_frame=self.merged_data,
+            db_name="pipeline",
+            table_name="co2_cars",
+        )
+
+    def run_pipeline(self):
+        # Extract
+        self.__extract()
+        # Transform
+        self.__transform()
+        # Merge
+        self.__merge()
+        # Load
+        self.__load()
+
+
+def __main__():
+    pipeline().run_pipeline()
