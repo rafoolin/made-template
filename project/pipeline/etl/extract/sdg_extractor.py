74767f76bfb419ca7c2bd3f258205e827030e403
Rafoolin <niloo.jv@gmail.com>
2023-11-21 00:54:50 +0100

    style: refactor and clean code

--- project/pipeline/etl/extract/sdg_extractor.py
+++ project/pipeline/etl/extract/sdg_extractor.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from etl.extract.zip_helper import GZipFileHelper
+
+from pipeline_utils.zip_helper import GZipFileHelper
 
 
 def sdg_data_extractor(compressed=True):
@@ -7,5 +8,3 @@ def sdg_data_extractor(compressed=True):
     url = f"https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data/sdg_12_30/?format=SDMX-CSV&compressed={compressed}"
     csv_file_path = zip_helper.download_and_extract_url_file(url)
     return pd.read_csv(csv_file_path)
-
-
--- project/pipeline/pipeline_utils/utils.py
+++ project/pipeline/pipeline_utils/utils.py
@@ -6,6 +6,3 @@ def get_directory_absolute_path():
     Return the absolute path of the root project based on this file.
     """
     return pathlib.Path(__file__).parent.parent.parent.parent.resolve()
-
-
-
