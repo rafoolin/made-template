0acc5bc536cef2b850028dab2969485c8a15f0a5
Rafoolin <niloo.jv@gmail.com>
2023-11-21 23:02:06 +0100

    style: review code

--- project/pipeline.sh
+++ project/pipeline.sh
@@ -5,20 +5,24 @@ NC='\033[0m'
 
 script_dir="$(dirname "$0")"
 echo -e "$GREEN starting...$NC"
+# Remove old files
 rm -f -r ".etl_pipeline"
 rm -f "./data/"*.csv "./data/"*.tsv "./data/"*.sqlite
 echo -e "$YELLOW Script directory name: $script_dir $NC"
 echo -e "$GREEN Install virtual environment(20.24.6) requirements...$NC"
+# Install virtual environment
 pip install virtualenv==20.24.6
 echo -e "$GREEN Create a virtual environment called [.etl_pipeline]...$NC"
 # The directory of the script
 virtualenv "$script_dir/.etl_pipeline"
 source "$script_dir/.etl_pipeline/bin/activate"
 echo -e "$GREEN Install requirements.txt...$NC"
+# Install "requirements.txt"
 pip install -r "$script_dir/pipeline/requirements.txt"
 echo -e "$GREEN All requirements are installed successfully!$NC"
 echo -e "$GREEN Pipeline started...$NC"
 python3 "$script_dir/pipeline/pipeline.py"
 echo -e "$GREEN Pipeline Finished$NC"
+# Deactivate virtual environment
 deactivate
 echo -e "$GREEN Deactivated the virtual environment$NC"
