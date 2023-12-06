9984a5d0fd13e8de7ed506d227ea38f6b850fb9e
Rafoolin <niloo.jv@gmail.com>
2023-12-06 01:50:04 +0100

    test(pipeline utils): add tests for utils

---
+++ project/tests/etl/pipeline_utils/utils_test.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+# TODO:: Add more unit tests for each util
+class TestGetDirectoryAbsolutePath(unittest.TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ project/tests/etl/pipeline_utils/zip_helper_test.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+# TODO:: Add more unit tests for each util
+class TestGZipFileHelper(unittest.TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
